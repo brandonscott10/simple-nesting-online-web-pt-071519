5c9184750d9a2e4e2200d910449acd47a36700fd
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-29 01:37:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,9 +87,9 @@ programmer_hash[:yukihiro_matsumoto] = {
     languages =>
       ["LISP", "C"]
     }
+    programmer_hash
 end
-def adding_matz
-end
+
 def changing_alan
 	programmer_hash = 
  		{
